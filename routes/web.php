933ba8cf17008dc2704e27149fed3556d53aca90
alexjohngarduque <alexanderjohngarduque@gmail.com>
2023-10-12 14:19:36 +0800

    v2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,15 +15,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//ROUTES = this is where you can give an url for your views, and access controller functions
-Route::get('/', function () { //route for landing page
-    return view('welcome');
+Route::get('/', function () {
+    return view('auth.login');
 });
 
-Route::get('/product',[ProductController::class, 'index'])->name('product.index'); //route for product page (display list of products)
-Route::get('/product/create',[ProductController::class, 'create'])->name('product.create'); //route for addding a new product page
-Route::post('/product/create',[ProductController::class, 'store'])->name('product.store'); //route that serves as a setter for new product
-Route::get('/product/{product}/edit',[ProductController::class, 'edit'])->name('product.edit'); //route for editing product details
-Route::put('/product/{product}/update',[ProductController::class, 'update'])->name('product.update'); //route for updating product details
-Route::delete('/product/{product}/delete',[ProductController::class, 'delete'])->name('product.delete'); //route for deleting a product
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/product',[ProductController::class, 'index'])->name('product.index'); //route for product page (display list of products)
+    Route::get('/product/create',[ProductController::class, 'create'])->name('product.create'); //route for addding a new product page
+    Route::post('/product/create',[ProductController::class, 'store'])->name('product.store'); //route that serves as a setter for new product
+    Route::get('/product/{product}/edit',[ProductController::class, 'edit'])->name('product.edit'); //route for editing product details
+    Route::put('/product/{product}/update',[ProductController::class, 'update'])->name('product.update'); //route for updating product details
+    Route::delete('/product/{product}/delete',[ProductController::class, 'delete'])->name('product.delete'); //route for deleting a product
+});
+
+require __DIR__.'/auth.php';
